526939adec82ba9e90cabca9d16163810f6fc50d
Lakshya Gupta <118376552+silentbang0@users.noreply.github.com>
2025-11-02 15:10:52 +0530

    Update training options in train_option.py

--- option/train_option.py
+++ option/train_option.py
@@ -3,7 +3,7 @@ import os
 def get_train_options():
     opt = {}
 
-    opt['project_dir'] = "/mnt/beegfs/haolin/PUGAN-pytorch"
+    opt['project_dir'] = "/kaggle/working/pugan_modified"
     opt['model_save_dir'] = opt['project_dir'] + '/checkpoints'
     opt["test_save_dir"]=opt['project_dir'] + '/test_results'
     opt['test_log_dir']=opt['project_dir'] + '/log_results'
@@ -12,11 +12,11 @@ def get_train_options():
     opt['train_split']=os.path.join(opt['project_dir'],'data','train_list.txt')
     opt['isTrain']=True
     opt['batch_size'] = 2
-    opt['nepoch'] = 100
-    opt['model_save_interval'] = 10
-    opt['model_vis_interval']=200
+    opt['nepoch'] = 20
+    opt['model_save_interval'] = 1
+    opt['model_vis_interval']=1
     opt["up_ratio"]=4
-    opt["patch_num_point"]=1024
+    opt["patch_num_point"]=256
     opt['lr_D']=1e-4
     opt['lr_G']=1e-3
     opt['emd_w']=100.0
